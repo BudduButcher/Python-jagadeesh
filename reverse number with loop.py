0442152f89c811befe7f3c91a3ee3e2f402d89ff
Jagadeesh <112267247+BudduButcher@users.noreply.github.com>
2022-09-30 13:32:25 +0530

    Add files via upload

--- reverse number with loop.py
+++ reverse number with loop.py
@@ -1,8 +1,16 @@
-n = int(input("Please Enter any Number: "))
-rev = 0
-while(n> 0):
-    rem = n %10
-    rev = (rev *10) + rem
-    n = n //10
-
-print("\n Reverse is = %d" %rev)
+try:
+   n=input("enter the number:")
+except:
+   ValueError
+   print("enter the proper value")
+else:
+   def reverse(n):
+       n1=""
+       for i in n:
+           if(i=="ch"):
+            print("invalid")
+           else:
+            n1=n1+i
+           return n1
+print("given string:",n)
+print("reverse string:",reverse(n))
